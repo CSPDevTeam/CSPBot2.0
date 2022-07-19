179f47a78468118abad47c62acf12a09818a86f0
twoone3 <3197653242@qq.com>
2022-07-20 07:51:04 +0800

    add gitignore

--- plugins/tester.py
+++
@@ -1,4 +0,0 @@
-import re
-lastLog = "12:49:12 INFO [Server] There are 0/10 players online:"
-matchObj = re.search(r"are\s(.+)\/(.+)\splayers",lastLog)
-print(matchObj.group(1),"/",matchObj.group(2))
